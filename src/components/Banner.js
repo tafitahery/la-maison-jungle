c203a0b20979317609bbc6093bcf2f4034f0b269
Tafita Hery <tafitahery@gmail.com>
2022-06-21 17:05:08 +0300

    Gagnez en temps et en efficacité grâce aux listes et aux conditions

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,14 @@
-import '../styles/Banner.css';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
 
 function Banner() {
-  const title = 'La maison jungle';
-  return (
-    <div className="lmj-banner">
-      <img src={logo} alt="La maison jungle" className="lmj-logo" />
-      <h1 className="lmj-title">{title}</h1>
-    </div>
-  );
+	const title = 'La maison jungle'
+	return (
+		<div className='lmj-banner'>
+			<img src={logo} alt='La maison jungle' className='lmj-logo' />
+			<h1 className='lmj-title'>{title}</h1>
+		</div>
+	)
 }
 
-export default Banner;
+export default Banner
